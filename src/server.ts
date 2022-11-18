ca5d3a91f64af2bdec0eee4ae65cb60134aba2a0
fomes <fgomesdeluna@gmail.com>
2022-11-18 19:16:43 -0300

    create transactions route

--- src/server.ts
+++ src/server.ts
@@ -60,6 +60,58 @@ app.get("/balance", verifyJWT, async (req, res) => {
   }
 });
 
+app.get("/transactions", verifyJWT, async (req, res) => {
+  const token = req.headers["authorization"];
+
+  const userData = jwt.decode(token as string);
+  const id = userData?.userId;
+
+  const user = await prisma.users.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).send({ message: "User not found!" });
+  } else {
+    const debitedAccount = await prisma.transactions.findFirst({
+      select: {
+        debitedAccountId: true,
+      },
+
+      where: {
+        debitedAccountId: user.accountId,
+      },
+    });
+
+    const creditedAccount = await prisma.transactions.findFirst({
+      select: {
+        creditedAccountId: true,
+      },
+
+      where: {
+        creditedAccountId: user.accountId,
+      },
+    });
+
+    if (debitedAccount || creditedAccount) {
+      const transaction = await prisma.transactions.findFirst({
+        where: {
+          OR: [
+            { creditedAccountId: creditedAccount?.creditedAccountId },
+            { debitedAccountId: debitedAccount?.debitedAccountId },
+          ],
+        },
+      });
+
+      return res.status(200).send({ transaction });
+    } else {
+      return res.status(400).send({ error: "No transactions found!" });
+    }
+  }
+});
+
 app.post("/cashout", verifyJWT, async (req, res) => {
   const token = req.headers["authorization"];
   const { userCashIn, amount } = req.body;
