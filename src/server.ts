8214615f0206c9d63ab93532a212a2d07ed514b2
fomes <fgomesdeluna@gmail.com>
2022-11-18 10:39:48 -0300

    create login route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+import bcrypt, { hash } from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -10,6 +11,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+const createPassword = async (password: string) => {
+  const saltRounds = 10;
+  const hashPassword = await bcrypt.hash(password, saltRounds);
+
+  return String(hashPassword);
+};
+
+const validatePassord = async (password: string, hashPassword: string) => {
+  const isMatch = await bcrypt.compare(password, hashPassword);
+
+  return isMatch;
+};
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Hello NG Cash!" });
@@ -42,6 +56,12 @@ app.post("/users/new", async (req, res) => {
     },
   });
 
+  if (username.length < 3) {
+    return res
+      .status(400)
+      .send({ message: "Username field must have at least 3 characters!" });
+  }
+
   if (password.length < 8) {
     return res
       .status(400)
@@ -51,15 +71,54 @@ app.post("/users/new", async (req, res) => {
   if (alreadyExists) {
     return res.status(403).send({ message: "User already exists!" });
   } else {
+    const pass = await createPassword(password);
+
     const newUser = await prisma.users.create({
       data: {
         username,
-        password,
+        password: pass,
         accountId: newAccount.id,
       },
     });
 
     return res.status(201).send({ message: "User successfuly created!" });
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const matchUser = await prisma.users.findFirst({
+    where: {
+      username,
+    },
+  });
+
+  if (matchUser) {
+    const hashPassword = await prisma.users.findFirst({
+      select: {
+        password: true,
+      },
+
+      where: {
+        username,
+      },
+    });
+
+    if (hashPassword) {
+      const matchPassword = await validatePassord(
+        password,
+        String(hashPassword.password)
+      );
+
+      if (matchPassword) {
+        return res.status(201).send({ message: "Successfuly login!" });
+      } else {
+        return res.status(400).send({ message: "Incorrect password!" });
+      }
+    }
+  } else {
+    return res.status(400).send({ message: "User not found!" });
   }
 });
 
